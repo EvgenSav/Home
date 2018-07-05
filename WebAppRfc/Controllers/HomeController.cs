2c066cffb4b3f19fba6b1cbd790cbb619626a2a6
Evgeny <imw1shlol@gmail.com>
2018-07-05 19:26:48 +0300

    Start of work

---
+++ WebAppRfc/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppRfc.Models;
+
+namespace WebAppRfc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string SwitchDev(int devKey) {
+            if(Program.DevBase.Data.ContainsKey(devKey)) {
+                Program.DevBase.Data[devKey].SetSwitch(Program.Mtrf64);
+            }
+            return LastRxMsg();
+        }
+
+        public string SetBright(int devKey, int bright) {
+            if (Program.DevBase.Data.ContainsKey(devKey)) {
+                Program.DevBase.Data[devKey].SetBright(Program.Mtrf64,bright);
+            }
+            return LastRxMsg();
+        }
+
+        public string LastRxMsg() {
+            return Program.Mtrf64.GetLogMsg(Program.Mtrf64.rxBuf);
+        }
+        public string DevBaseList() {
+            string res = "";
+            foreach (var item in Program.DevBase.Data) {
+                res += String.Format("Name: {0} key: {1} \n",item.Value.Name,item.Key);
+            }
+            return res;
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppRfc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RFController;
+
+namespace WebAppRfc
+{
+    public class Program
+    {
+        public static MTRF Mtrf64;
+        public static MyDB<int, RfDevice> DevBase;
+
+        public static void Main(string[] args)
+        {
+            DevBase = MyDB<int, RfDevice>.OpenFile("devices.json");
+            Mtrf64 = new MTRF();
+            List<Mtrf> availableAdapters = Mtrf64.GetAvailableComPorts();
+            if (availableAdapters.Count > 0) {
+                Mtrf64.OpenPort(availableAdapters[0]);
+            }
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
